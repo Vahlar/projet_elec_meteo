19c8af50cf48a20d742029d139f2f9401bf8ef8d
LucileComba <mlle.lucile@hotmail.fr>
2020-12-04 09:16:57 +0100

    -Modification fonctionnelle du code de l'anémometre -Modification fonctionnelle du code du pluviometre -Changement de l'image de l'oscillo

--- Lucile_Comba/Anemometre/Anemometre.ino
+++ Lucile_Comba/Anemometre/Anemometre.ino
@@ -6,11 +6,12 @@
 # define PinA 35
 
 ///--- VARIABLES 
-int nbRotation = 0;            //number of rotation
-long measurementPeriod = 1500;           //sampling period in ms
-unsigned long instant = 0;              //current time
-unsigned long previous_millis = 0;        //stores the instant variable
-float Speed = 0;                        //wind speed
+int nbInterrupt = 0;   //number of interruption & interruption per second
+long measurementPeriod = 1000;           //sampling period in ms
+unsigned long instant = 0;               //current time
+unsigned long previous_loop = 0;         //time of the last loop
+unsigned long previous_interrupt = 0;    //time of the last interruption
+float Speed = 0, interruptSec = 0;       //wind speed
 
 ///--- FUNCTIONS
 void launchAnemometerProcess();
@@ -27,7 +28,7 @@ void setup()
   Serial.println();
 
   //at each lap: call of the function to increment the number of rotation
-  attachInterrupt(digitalPinToInterrupt(PinA), launchAnemometerProcess, RISING);
+  attachInterrupt(digitalPinToInterrupt(PinA), launchAnemometerProcess, FALLING);
 
 }
 
@@ -36,23 +37,29 @@ void loop()
   instant = millis();
   //Serial.println(nbRotation);
   
-  if ((instant - previous_millis) >= measurementPeriod)     //if the measurement period has passed
+  if ((instant - previous_loop) >= measurementPeriod)     //if the measurement period has passed
   {
-    Speed = (nbRotation / (measurementPeriod * 0.001)) * 2.4;   //A wind speed of 2.4 km/h causes the switch to close once per second
-                                                                // measurementPeriod * 0.001 is used to define interrupt/sec depending on the periode
+    interruptSec = nbInterrupt / (measurementPeriod * 0.001);   //A wind speed of 2.4 km/h causes the switch to close once per second
+    Speed = interruptSec*2.4;                                  // measurementPeriod * 0.001 is used to define interrupt/sec depending on the periode              
+                                                                
     //Display of the speed
-    Serial.println(nbRotation);
+    //Serial.println(nbInterrupt);
+    //Serial.println(interruptSec);
     Serial.print("wind speed: ");   
     Serial.print(Speed);                                  
     Serial.println(" km/h");
     
-    previous_millis = instant;     //resets the time and counter for the next measurement period
-    nbRotation = 0;
+    previous_loop = instant;     //resets the time and counter for the next measurement period
+    nbInterrupt = 0;
    }
   
 }
 
 void launchAnemometerProcess()                                                             
 {
-  nbRotation = nbRotation + 1;                //increment of the number of rotations
+  if(previous_interrupt == 0 || instant-previous_interrupt >= 5) // if it is the first interruption or the next one after 5 ms (debouncing)
+  {
+    previous_interrupt = instant;
+    nbInterrupt = nbInterrupt + 1;  //increment of the number of interruptions
+  }
 }
--- Lucile_Comba/pluviometre/pluviometre.ino
+++ Lucile_Comba/pluviometre/pluviometre.ino
@@ -6,10 +6,12 @@
 # define PinP 34
 
 ///--- VARIABLES 
-unsigned long nbRocker = 0;           //number of rockers
-long measurementPeriod = 1500;            //sampling period in ms
-unsigned long instant = 0;            //current time
-unsigned long previous_millis = 0;            //stores the instant variable            
+unsigned long nbRocker = 0;             //number of rockers
+float qtyRain = 0;             //quantity of rain (mm)
+long measurementPeriod = 5000;          //sampling period in ms
+unsigned long instant = 0;              //current time
+unsigned long previous_loop = 0;        //time of the last loop
+unsigned long previous_interrupt = 0;   //time of the last interruption            
 
 ///--- FUNCTIONS
 void launchRainGaugeProcess();
@@ -20,23 +22,29 @@ void setup()
 {
   pinMode(PinP, INPUT_PULLDOWN);
   Serial.begin(9600);
+  Serial.println("");
   while (!Serial);
 
   //at each tilting: call of the function to increment the number of rockers
-  attachInterrupt(digitalPinToInterrupt(PinP), launchRainGaugeProcess, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PinP), launchRainGaugeProcess, FALLING);
 
 }
 
 void loop()
 {
   instant = millis();
-  if ((instant - previous_millis) >= measurementPeriod)     //if the measurement period has passed
+  if ((instant - previous_loop) >= measurementPeriod)     //if the measurement period has passed
   {
-    // Display of the number of rockers
+    // Calculate the quantity of rain on the period
+    qtyRain = 0.2794*nbRocker;
+    // Display of the number of rockers & the rain quantity
     Serial.print("Number of rockers : ");
     Serial.println(nbRocker);
+    Serial.print("Quantity of rain : ");
+    Serial.print(qtyRain);    
+    Serial.println(" L/m²");
     
-    previous_millis = instant;    //resets the time and counter for the next measurement period
+    previous_loop = instant;    //resets the time and counter for the next measurement period
     nbRocker = 0;
    }
   
@@ -44,5 +52,9 @@ void loop()
 
 void launchRainGaugeProcess()                  
 {                                      
-  nbRocker += 1;          //increment of the number of rockers
+  if(previous_interrupt == 0 || instant-previous_interrupt >= 5) // if it is the first interruption or the next one after 5 ms (debouncing)
+  {
+    previous_interrupt = instant;
+    nbRocker += 1;          //increment of the number of rockers
+  }
 }
