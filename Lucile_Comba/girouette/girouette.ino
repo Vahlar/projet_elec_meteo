a8c0281b6767bb137036543aa9e8db838eea4ad7
LucileComba <mlle.lucile@hotmail.fr>
2021-04-01 16:07:44 +0200

    MAJ code Girouette

--- Lucile_Comba/girouette/girouette.ino
+++ Lucile_Comba/girouette/girouette.ino
@@ -1,15 +1,18 @@
 #include <Wire.h> 
 #include <SPI.h>
 #include <driver/adc.h>
+//#include "esp_adc_cal.h"
 
 ///--- Définitions des PINS
-# define PinG 32 // ->32-39
+# define PinG 34// ->32-39
 
 ///--- VARIABLES 
 long measurementPeriod = 1000;           //sampling period in ms
 unsigned long instant = 0;               //current time
 unsigned long previous_loop = 0;         //time of the last loop
-float Direction = 0, tension = 0;       //wind direction
+float tension = 0, tension_alim = 0;       //tensions
+float coef_ajustement = 0;
+long sum;
 
 ///--- FUNCTIONS
 
@@ -18,7 +21,7 @@ float Direction = 0, tension = 0;       //wind direction
 void setup()
 {
   
-  pinMode(PinG, INPUT);
+  //pinMode(PinG, INPUT);
   Serial.begin(9600);
   while (!Serial);
   Serial.println();
@@ -31,15 +34,33 @@ void loop()
   
   if ((instant - previous_loop) >= measurementPeriod)     //if the measurement period has passed
   {
-    //analogSetAttenuation(ADC_11db);                                           
-    //tension = analogRead(PinG);
-    //Serial.println(tension); 
-
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_4,ADC_ATTEN_DB_11);
-    tension = adc1_get_raw(ADC1_CHANNEL_0);
+    //adc_power_on();
+    //adc1_config_width(ADC_WIDTH_12Bit);
+    //adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11);
+    
+    sum = 0;
+    for (int i = 0; i<20; i++) {
+      //tension = adc1_get_raw(ADC1_CHANNEL_6);
+      tension = analogRead(PinG);
+      sum += tension;
+    }
+    
+    tension = sum / 20;
+    
+    void analogReference(DEFAULT);   //tension d'alimentation réel
+    coef_ajustement = 3.3/tension_alim;     //coef de variation par rapport à la tension attendu d'alimentation 3,3
+   
+    Serial.println(float((tension * 3.9)/4095));
+   
+    
+    /*tension = adc1_get_raw(ADC1_CHANNEL_6);
+    Serial.println(tension);
+    tension = (tension * 3.9)/4095;
     Serial.println(tension); 
-                                                              
+    
+    tension = analogRead(PinG); Serial.print("analog : ");
+    Serial.println((tension* 3.9)/4095); 
+    */                                                    
     //Display of the direction
     //Serial.print("wind direction: ");   
     //Serial.println(Direction);                                  
